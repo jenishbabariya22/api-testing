2cb20eaae8e0bde172c0e854a675ea8edd13b39e
jenishbabariya22 <jenishbabariya58@gmail.com>
2024-10-16 09:42:10 +0530

    new

--- server.js
+++ server.js
@@ -10,9 +10,13 @@ const app = express();
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI)
-.then(() => console.log("MongoDB connected"))
-.catch((err) => console.error("MongoDB connection error:", err));
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('MongoDB connected successfully'))
+  .catch((err) => console.error('MongoDB connection error:', err));
+  
 
 // Create a basic data schema
 const DataSchema = new mongoose.Schema({
